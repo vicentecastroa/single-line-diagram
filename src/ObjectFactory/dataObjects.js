b563a3003993a890141765e255da1e023dca01d0
Vicente Castro <vicente.castro@acelerex.com>
2021-11-29 13:15:38 -0300

    log

--- src/ObjectFactory/dataObjects.js
+++ src/ObjectFactory/dataObjects.js
@@ -1,6 +1,7 @@
 import { NETWORK } from "../Network";
 //No separate namespace has been  created for the Object Factory class as the NETWORK OBJECT should itself have an Object Factory that generates and/or updates the DataObjects.
 export default (function () {
+  console.log("NETWORK", NETWORK);
   NETWORK.ObjectFactory = function () {
     var dataObjects = ["data Objects"];
     this.getNetworkDataObjects = function () {
