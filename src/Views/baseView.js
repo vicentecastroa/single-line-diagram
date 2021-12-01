b9a352d10ff8fbcd8e837161bf302a3ac5a8515c
Vicente Castro <vicente.castro@acelerex.com>
2021-12-01 10:24:00 -0300

    var def

--- src/Views/baseView.js
+++ src/Views/baseView.js
@@ -1,16 +1,16 @@
 import $ from "jquery";
 import * as d3 from "d3";
 
-var zoom = d3.zoom();
-var zoomTransform = d3.zoomTransform();
+const zoom = d3.zoom();
+const zoomTransform = d3.zoomTransform();
 
 const graphBounds = (withCola, myCola) => {
-  var x = Number.POSITIVE_INFINITY,
-    X = Number.NEGATIVE_INFINITY,
-    y = Number.POSITIVE_INFINITY,
-    Y = Number.NEGATIVE_INFINITY;
+  let x = Number.POSITIVE_INFINITY;
+  let X = Number.NEGATIVE_INFINITY;
+  let y = Number.POSITIVE_INFINITY;
+  let Y = Number.NEGATIVE_INFINITY;
   //Check the visibility of the graph and then Select the graph for which the layout is to be exported.
-  var selCola;
+  let selCola;
   if ($("#parentSvgNode").is(":visible")) {
     selCola = myCola;
   } else {
@@ -60,15 +60,15 @@ const SharedFunctionality = {
   goToInitialStateTriggered: false,
 
   zoomToFit: (withCola, myCola) => {
-    var cw = window.innerWidth * 0.98 - 160 - SharedFunctionality.R * 2;
-    var ch = window.innerHeight * 0.85 + SharedFunctionality.R * 1;
-    var b = graphBounds(withCola, myCola);
-    var w = b.X - b.x,
+    const cw = window.innerWidth * 0.98 - 160 - SharedFunctionality.R * 2;
+    const ch = window.innerHeight * 0.85 + SharedFunctionality.R * 1;
+    const b = graphBounds(withCola, myCola);
+    const w = b.X - b.x,
       h = b.Y - b.y;
-    var s = Math.min(cw / w, ch / h);
-    var tx = -b.x * s + ((cw / s - w) * s) / 2;
-    var ty = -b.y * s + ((ch / s - h) * s) / 2;
-    var selZoom;
+    const s = Math.min(cw / w, ch / h);
+    const tx = -b.x * s + ((cw / s - w) * s) / 2;
+    const ty = -b.y * s + ((ch / s - h) * s) / 2;
+    let selZoom;
     if ($("#parentSvgNode").is(":visible")) {
       selZoom = zoom;
     } else {
