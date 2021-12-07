e9ab5ae897aeeeebc4f9f6651b20ed5a3432cd78
Vicente Castro <vicente.castro@acelerex.com>
2021-12-07 12:29:02 -0300

    fix: remove graph before drawing it

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import * as d3 from "d3";
 import ObjectFactoryClass from "./ObjectFactory/dataObjects";
 import DisconnectedGraph from "./Graph/disconnectedGraph";
 
@@ -6,15 +7,21 @@ let myCola;
 
 function drawGraph(event) {
   myCola = {};
-  console.log(event);
+  preProcessNetworkUI();
 
   const ObjectFactory = new ObjectFactoryClass(event);
 
   NETWORK_OBJECTS = ObjectFactory.getNetworkDataObjects();
   /*Logging NETWORK_OBJECTS for reference purpose.*/
-  console.log("NETWORK OBJECTS", NETWORK_OBJECTS);
+  //console.log("NETWORK OBJECTS", NETWORK_OBJECTS);
 
   DisconnectedGraph();
+}
+
+function preProcessNetworkUI() {
+  if (d3.select("#parentSvgNode")._groups[0][0]) {
+    d3.select("#parentSvgNode").remove();
+  }
 }
 
 export { NETWORK_OBJECTS, myCola };
