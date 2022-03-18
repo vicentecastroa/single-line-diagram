2f954f865c53440f15b89ed0c501431480529add
Vicente Castro <vicente.castro@acelerex.com>
2022-03-18 11:24:47 -0300

    feat: ev chargers

--- src/Graph/Nodes/Decorators/BottomDecorators.js
+++ src/Graph/Nodes/Decorators/BottomDecorators.js
@@ -9,6 +9,7 @@ import generatorSolarIcon from "../../../Icons/generatorSolar";
 import generatorThermalIcon from "../../../Icons/generatorThermal";
 import generatorHydroIcon from "../../../Icons/generatorHydro";
 import generatorWindIcon from "../../../Icons/generatorWind";
+import evIcon from '../../../Icons/ev;'
 
 const parser = new DOMParser();
 
@@ -22,6 +23,7 @@ function BottomDecorators(nodesGroupTag) {
     generatorThermal: generatorThermalIcon,
     generatorHydro: generatorHydroIcon,
     generatorWind: generatorWindIcon,
+    ev: evIcon
   };
 }
 
