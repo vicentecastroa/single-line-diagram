b7bc14e69772cbfe399605beeca101033f4779ff
Vicente Castro <vicente.castro@acelerex.com>
2022-04-14 10:35:04 -0400

    fix: multi bus decorators

--- src/Graph/Nodes/Decorators/BottomDecorators.js
+++ src/Graph/Nodes/Decorators/BottomDecorators.js
@@ -9,7 +9,7 @@ import generatorSolarIcon from "../../../Icons/generatorSolar";
 import generatorThermalIcon from "../../../Icons/generatorThermal";
 import generatorHydroIcon from "../../../Icons/generatorHydro";
 import generatorWindIcon from "../../../Icons/generatorWind";
-import evIcon from '../../../Icons/ev';
+import evIcon from "../../../Icons/ev";
 
 const parser = new DOMParser();
 
@@ -23,213 +23,221 @@ function BottomDecorators(nodesGroupTag) {
     generatorThermal: generatorThermalIcon,
     generatorHydro: generatorHydroIcon,
     generatorWind: generatorWindIcon,
-    ev: evIcon
+    ev: evIcon,
   };
 }
 
 BottomDecorators.prototype.decorate = function () {
-  this.nodesGroupTag._groups.forEach((d) => {
-    const nodeGroup = d[0].__data__;
-    const bottomDecorators = nodeGroup.bottomDecorators;
-    const bottomDecoCount = bottomDecorators.length;
-    const R = SharedFunctionality.R;
-    const LL = R / 2;
-    const decoratorY = 3 * R;
-    const decoratorWidth = 2 * R;
-    const loadWidth = decoratorWidth * 0.6;
+  console.log(this.nodesGroupTag);
+  this.nodesGroupTag._groups.forEach((g) => {
+    g.forEach((d) => {
+      const nodeGroup = d.__data__;
+      const bottomDecorators = nodeGroup.bottomDecorators;
+      const bottomDecoCount = bottomDecorators.length;
+      const R = SharedFunctionality.R;
+      const LL = R / 2;
+      const decoratorY = 3 * R;
+      const decoratorWidth = 2 * R;
+      const loadWidth = decoratorWidth * 0.6;
+      const bottomDecoratorGroup = d3
+        .select(d)
+        .append("g")
+        .attr("class", "bottomDecoratorGroup")
+        .attr("id", () => bottomDecorators.DOMID);
 
-    const bottomDecoratorGroup = d3
-      .select(d[0])
-      .append("g")
-      .attr("class", "bottomDecoratorGroup")
-      .attr("id", () => bottomDecorators.DOMID);
+      if (bottomDecoCount !== 0) {
+        // Adding vertical central connector.
+        bottomDecoratorGroup
+          .append("line")
+          .attr("class", "connectors")
+          .attr("x1", 0)
+          .attr("x2", 0)
+          .attr("y1", LL)
+          .attr("y2", () => (bottomDecoCount > 1 ? R + LL : decoratorY));
 
-    if (bottomDecoCount !== 0) {
-      // Adding vertical central connector.
-      bottomDecoratorGroup
-        .append("line")
-        .attr("class", "connectors")
-        .attr("x1", 0)
-        .attr("x2", 0)
-        .attr("y1", LL)
-        .attr("y2", () => (bottomDecoCount > 1 ? R + LL : decoratorY));
+        for (let index = 0; index < bottomDecoCount; index++) {
+          const decorator = bottomDecorators[index];
+          if (decorator.resourceType !== "load") {
+            const icon = parser.parseFromString(
+              this.icons[decorator.resourceType],
+              "image/svg+xml"
+            );
 
-      for (let index = 0; index < bottomDecoCount; index++) {
-        const decorator = bottomDecorators[index];
-        if (decorator.resourceType !== "load") {
-          const icon = parser.parseFromString(
-            this.icons[decorator.resourceType],
-            "image/svg+xml"
-          );
+            const decoratorHTML = bottomDecoratorGroup
+              .node()
+              .appendChild(icon.documentElement);
 
-          const decoratorHTML = bottomDecoratorGroup
-            .node()
-            .appendChild(icon.documentElement);
+            d3.select(decoratorHTML)
+              .attr("width", decoratorWidth)
+              .attr("height", decoratorWidth)
+              .attr("id", () => `bus${nodeGroup.id}bottomDeco${index}`)
+              .attr("y", decoratorY)
+              .attr("x", () => {
+                if (bottomDecoCount % 2 === 0) {
+                  //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
+                  const x = (bottomDecoCount - 4) / 2 + 0.5;
+                  return (-(bottomDecoCount + x) + 3 * index) * R - R;
+                } else
+                  return (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R - R;
+              })
+              .on("mouseover", ($event) => {
+                showTooltip(decorator, $event, "");
+              })
+              .on("mouseout", () => hideTooltip());
+          } else {
+            bottomDecoratorGroup
+              .append("svg:defs")
+              .append("svg:marker")
+              .attr("id", () => `Arrowhead${nodeGroup.id}`)
+              .attr("refx", 0)
+              .attr("refY", 5)
+              .attr("markerWidth", 10)
+              .attr("markerHeight", 10)
+              .attr("markerUnits", "strokeWidth")
+              .attr("orient", "auto")
+              .attr("class", "loadMarker")
+              .append("svg:polygon")
+              .attr("points", "0 1, 9 5, 0 9")
+              .attr("class", "loadPolygon");
 
-          d3.select(decoratorHTML)
-            .attr("width", decoratorWidth)
-            .attr("height", decoratorWidth)
-            .attr("id", () => `bus${nodeGroup.id}bottomDeco${index}`)
-            .attr("y", decoratorY)
-            .attr("x", () => {
-              if (bottomDecoCount % 2 === 0) {
-                //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
-                const x = (bottomDecoCount - 4) / 2 + 0.5;
-                return (-(bottomDecoCount + x) + 3 * index) * R - R;
-              } else
-                return (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R - R;
-            })
-            .on("mouseover", ($event) => {
-              showTooltip(decorator, $event, "");
-            })
-            .on("mouseout", () => hideTooltip());
-        } else {
-          bottomDecoratorGroup
-            .append("svg:defs")
-            .append("svg:marker")
-            .attr("id", () => `Arrowhead${nodeGroup.id}`)
-            .attr("refx", 0)
-            .attr("refY", 5)
-            .attr("markerWidth", 10)
-            .attr("markerHeight", 10)
-            .attr("markerUnits", "strokeWidth")
-            .attr("orient", "auto")
-            .attr("class", "loadMarker")
-            .append("svg:polygon")
-            .attr("points", "0 1, 9 5, 0 9")
-            .attr("class", "loadPolygon");
+            bottomDecoratorGroup
+              .append("line")
+              .attr("id", () => `bus${nodeGroup.id}bottomDeco${index}`)
+              .attr("class", "connectors")
+              .attr("x1", () => {
+                if (bottomDecoCount % 2 === 0) {
+                  //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
+                  const x = (bottomDecoCount - 4) / 2 + 0.5;
+                  return (-(bottomDecoCount + x) + 3 * index) * R - R;
+                } else
+                  return (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R - R;
+              })
+              .attr("x2", () => {
+                if (bottomDecoCount % 2 === 0) {
+                  //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
+                  const x = (bottomDecoCount - 4) / 2 + 0.5;
+                  return (-(bottomDecoCount + x) + 3 * index) * R - R;
+                } else
+                  return (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R - R;
+              })
+              .attr("y1", decoratorY)
+              .attr("y2", decoratorY + R / 2)
+              .attr("marker-end", `url(#Arrowhead${nodeGroup.id})`);
 
-          bottomDecoratorGroup
-            .append("line")
-            .attr("id", () => `bus${nodeGroup.id}bottomDeco${index}`)
-            .attr("class", "connectors")
-            .attr("x1", () => {
-              if (bottomDecoCount % 2 === 0) {
-                //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
-                const x = (bottomDecoCount - 4) / 2 + 0.5;
-                return (-(bottomDecoCount + x) + 3 * index) * R - R;
-              } else
-                return (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R - R;
-            })
-            .attr("x2", () => {
-              if (bottomDecoCount % 2 === 0) {
-                //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
-                const x = (bottomDecoCount - 4) / 2 + 0.5;
-                return (-(bottomDecoCount + x) + 3 * index) * R - R;
-              } else
-                return (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R - R;
-            })
-            .attr("y1", decoratorY)
-            .attr("y2", decoratorY + R / 2)
-            .attr("marker-end", `url(#Arrowhead${nodeGroup.id})`);
+            // Transparent rectangle to handle hover tooltip
+            bottomDecoratorGroup
+              .append("rect")
+              .attr("width", loadWidth)
+              .attr("height", loadWidth * 1.2)
+              .attr("y", decoratorY)
+              .attr("x", () => {
+                if (bottomDecoCount % 2 === 0) {
+                  //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
+                  const x = (bottomDecoCount - 4) / 2 + 0.5;
+                  return (
+                    (-(bottomDecoCount + x) + 3 * index) * R - R - loadWidth / 2
+                  );
+                } else
+                  return (
+                    (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R -
+                    R -
+                    loadWidth / 2
+                  );
+              })
+              .attr("style", `fill:transparent`)
+              .on("mouseover", ($event) => {
+                showTooltip(decorator, $event, "");
+              })
+              .on("mouseout", () => hideTooltip());
+          }
 
-          // Transparent rectangle to handle hover tooltip
+          // Adding connecting lines (vertical lines) for multiple top decorators.
+          const y1 = decoratorY; // 1.4 is factor for margin
+          const y2 = R + LL;
+          if (bottomDecoCount > 1) {
+            bottomDecoratorGroup
+              .append("line")
+              .attr("class", "connectors")
+              .attr("x1", () =>
+                decorator.resourceType === "load"
+                  ? $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x1")
+                  : Number(
+                      $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x")
+                    ) +
+                    decoratorWidth / 2
+              )
+              .attr("x2", () =>
+                decorator.resourceType === "load"
+                  ? $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x1")
+                  : Number(
+                      $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x")
+                    ) +
+                    decoratorWidth / 2
+              )
+              .attr("y1", y1)
+              .attr("y2", y2 - 0.5)
+              .attr(
+                "dx",
+                () => $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x") - 4
+              );
+          }
+
+          // Adding breaker to vertical lines
+          const breakerWidth = 8;
+          const breakerHeight = breakerWidth;
+          const breakerFillColor =
+            bottomDecorators[index].bottomDecoData.breaker === "open"
+              ? "white"
+              : "black";
           bottomDecoratorGroup
             .append("rect")
-            .attr("width", loadWidth)
-            .attr("height", loadWidth * 1.2)
-            .attr("y", decoratorY)
-            .attr("x", () => {
-              if (bottomDecoCount % 2 === 0) {
-                //Factor to be added to the bottomDecoCount to adjust the position of the bottom decorators.
-                const x = (bottomDecoCount - 4) / 2 + 0.5;
-                return (
-                  (-(bottomDecoCount + x) + 3 * index) * R - R - loadWidth / 2
-                );
-              } else
-                return (
-                  (-(3 * (bottomDecoCount - 1)) / 2 + 3 * index) * R -
-                  R -
-                  loadWidth / 2
-                );
-            })
-            .attr("style", `fill:transparent`)
-            .on("mouseover", ($event) => {
-              showTooltip(decorator, $event, "");
-            })
-            .on("mouseout", () => hideTooltip());
+            .attr("class", "connectors")
+            .attr("id", `bus${nodeGroup.id}bottomDeco${index}Breaker`)
+            .attr(
+              "x",
+              () =>
+                (decorator.resourceType === "load"
+                  ? $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x1")
+                  : Number(
+                      $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x")
+                    ) +
+                    decoratorWidth / 2) -
+                breakerWidth / 2
+            )
+            .attr("y", y1 - breakerHeight * 1.8)
+            .attr("width", breakerWidth)
+            .attr("height", breakerHeight)
+            .attr("style", `fill:${breakerFillColor}`);
         }
 
-        // Adding connecting lines (vertical lines) for multiple top decorators.
-        const y1 = decoratorY; // 1.4 is factor for margin
-        const y2 = R + LL;
+        // Adding horizontal central connector for multiple top decorators.
         if (bottomDecoCount > 1) {
           bottomDecoratorGroup
             .append("line")
             .attr("class", "connectors")
-            .attr("x1", () =>
-              decorator.resourceType === "load"
-                ? $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x1")
-                : Number($(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x")) +
-                  decoratorWidth / 2
+            .attr(
+              "x1",
+              () =>
+                Number($(`#bus${nodeGroup.id}bottomDeco0`).attr("x")) +
+                decoratorWidth / 2
             )
             .attr("x2", () =>
-              decorator.resourceType === "load"
-                ? $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x1")
-                : Number($(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x")) +
+              bottomDecorators[bottomDecoCount - 1].resourceType === "load"
+                ? $(`#bus${nodeGroup.id}bottomDeco${bottomDecoCount - 1}`).attr(
+                    "x1"
+                  )
+                : Number(
+                    $(
+                      `#bus${nodeGroup.id}bottomDeco${bottomDecoCount - 1}`
+                    ).attr("x")
+                  ) +
                   decoratorWidth / 2
             )
-            .attr("y1", y1)
-            .attr("y2", y2 - 0.5)
-            .attr(
-              "dx",
-              () => $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x") - 4
-            );
+            .attr("y1", R + LL)
+            .attr("y2", R + LL);
         }
-
-        // Adding breaker to vertical lines
-        const breakerWidth = 8;
-        const breakerHeight = breakerWidth;
-        const breakerFillColor =
-          bottomDecorators[index].bottomDecoData.breaker === "open"
-            ? "white"
-            : "black";
-        bottomDecoratorGroup
-          .append("rect")
-          .attr("class", "connectors")
-          .attr('id', `bus${nodeGroup.id}bottomDeco${index}Breaker`)
-          .attr(
-            "x",
-            () =>
-              (decorator.resourceType === "load"
-                ? $(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x1")
-                : Number($(`#bus${nodeGroup.id}bottomDeco${index}`).attr("x")) +
-                  decoratorWidth / 2) -
-              breakerWidth / 2
-          )
-          .attr("y", y1 - breakerHeight * 1.8)
-          .attr("width", breakerWidth)
-          .attr("height", breakerHeight)
-          .attr("style", `fill:${breakerFillColor}`);
       }
-
-      // Adding horizontal central connector for multiple top decorators.
-      if (bottomDecoCount > 1) {
-        bottomDecoratorGroup
-          .append("line")
-          .attr("class", "connectors")
-          .attr(
-            "x1",
-            () =>
-              Number($(`#bus${nodeGroup.id}bottomDeco0`).attr("x")) +
-              decoratorWidth / 2
-          )
-          .attr("x2", () =>
-            bottomDecorators[bottomDecoCount - 1].resourceType === "load"
-              ? $(`#bus${nodeGroup.id}bottomDeco${bottomDecoCount - 1}`).attr(
-                  "x1"
-                )
-              : Number(
-                  $(`#bus${nodeGroup.id}bottomDeco${bottomDecoCount - 1}`).attr(
-                    "x"
-                  )
-                ) +
-                decoratorWidth / 2
-          )
-          .attr("y1", R + LL)
-          .attr("y2", R + LL);
-      }
-    }
+    });
   });
 };
 
--- src/Graph/Nodes/Decorators/TopDecorators.js
+++ src/Graph/Nodes/Decorators/TopDecorators.js
@@ -18,62 +18,112 @@ function TopDecorators(nodesGroupTag) {
 }
 
 TopDecorators.prototype.decorate = function () {
-  this.nodesGroupTag._groups.forEach((d) => {
-    const nodeGroup = d[0].__data__;
-    const topDecorators = nodeGroup.topDecorators;
-    const topDecoCount = topDecorators.length;
-    const R = SharedFunctionality.R;
-    const LL = R / 2;
-    const decoratorY = -5 * R;
-    const decoratorWidth = 2 * R;
+  this.nodesGroupTag._groups.forEach((g) => {
+    g.forEach((d) => {
+      const nodeGroup = d.__data__;
+      const topDecorators = nodeGroup.topDecorators;
+      const topDecoCount = topDecorators.length;
+      const R = SharedFunctionality.R;
+      const LL = R / 2;
+      const decoratorY = -5 * R;
+      const decoratorWidth = 2 * R;
 
-    const topDecoratorGroup = d3
-      .select(d[0])
-      .append("g")
-      .attr("class", "topDecoratorGroup")
-      .attr("id", () => topDecorators.DOMID);
+      const topDecoratorGroup = d3
+        .select(d)
+        .append("g")
+        .attr("class", "topDecoratorGroup")
+        .attr("id", () => topDecorators.DOMID);
 
-    if (topDecoCount !== 0) {
-      // Adding vertical central connector.
-      topDecoratorGroup
-        .append("line")
-        .attr("class", "connectors")
-        .attr("x1", 0)
-        .attr("x2", 0)
-        .attr("y1", -LL)
-        .attr("y2", () =>
-          topDecoCount > 1 ? -(R + LL) : decoratorY + R + LL * 1.4
-        );
+      if (topDecoCount !== 0) {
+        // Adding vertical central connector.
+        topDecoratorGroup
+          .append("line")
+          .attr("class", "connectors")
+          .attr("x1", 0)
+          .attr("x2", 0)
+          .attr("y1", -LL)
+          .attr("y2", () =>
+            topDecoCount > 1 ? -(R + LL) : decoratorY + R + LL * 1.4
+          );
 
-      for (let index = 0; index < topDecoCount; index++) {
-        const decorator = topDecorators[index];
-        const icon = parser.parseFromString(
-          this.icons[decorator.resourceType],
-          "image/svg+xml"
-        );
-        const decoratorHTML = topDecoratorGroup
-          .node()
-          .appendChild(icon.documentElement);
-        d3.select(decoratorHTML)
-          .attr("width", decoratorWidth)
-          .attr("height", decoratorWidth)
-          .attr("id", () => `bus${nodeGroup.id}topDeco${index}`)
-          .attr("y", decoratorY)
-          .attr("x", () => {
-            if (topDecoCount % 2 === 0) {
-              //Factor to be added to the topDecoCount to adjust the position of the top decorators.
-              const x = (topDecoCount - 4) / 2 + 0.5;
-              return (-(topDecoCount + x) + 3 * index) * R - R;
-            } else return (-(3 * (topDecoCount - 1)) / 2 + 3 * index) * R - R;
-          })
-          .on("mouseover", ($event) => {
-            showTooltip(decorator, $event, "");
-          })
-          .on("mouseout", () => hideTooltip());
+        for (let index = 0; index < topDecoCount; index++) {
+          const decorator = topDecorators[index];
+          const icon = parser.parseFromString(
+            this.icons[decorator.resourceType],
+            "image/svg+xml"
+          );
+          const decoratorHTML = topDecoratorGroup
+            .node()
+            .appendChild(icon.documentElement);
+          d3.select(decoratorHTML)
+            .attr("width", decoratorWidth)
+            .attr("height", decoratorWidth)
+            .attr("id", () => `bus${nodeGroup.id}topDeco${index}`)
+            .attr("y", decoratorY)
+            .attr("x", () => {
+              if (topDecoCount % 2 === 0) {
+                //Factor to be added to the topDecoCount to adjust the position of the top decorators.
+                const x = (topDecoCount - 4) / 2 + 0.5;
+                return (-(topDecoCount + x) + 3 * index) * R - R;
+              } else return (-(3 * (topDecoCount - 1)) / 2 + 3 * index) * R - R;
+            })
+            .on("mouseover", ($event) => {
+              showTooltip(decorator, $event, "");
+            })
+            .on("mouseout", () => hideTooltip());
 
-        // Adding connecting lines (vertical lines) for multiple top decorators.
-        var y1 = decoratorY + R + LL * 1.4; // 1.4 is factor for margin
-        var y2 = -(R + LL);
+          // Adding connecting lines (vertical lines) for multiple top decorators.
+          var y1 = decoratorY + R + LL * 1.4; // 1.4 is factor for margin
+          var y2 = -(R + LL);
+          if (topDecoCount > 1) {
+            topDecoratorGroup
+              .append("line")
+              .attr("class", "connectors")
+              .attr(
+                "x1",
+                () =>
+                  Number($(`#bus${nodeGroup.id}topDeco${index}`).attr("x")) +
+                  decoratorWidth / 2
+              )
+              .attr(
+                "x2",
+                () =>
+                  Number($(`#bus${nodeGroup.id}topDeco${index}`).attr("x")) +
+                  decoratorWidth / 2
+              )
+              .attr("y1", y1)
+              .attr("y2", y2 + 0.5)
+              .attr(
+                "dx",
+                () => $(`#bus${nodeGroup.id}topDeco${index}`).attr("x") - 4
+              );
+          }
+
+          // Adding breaker to vertical lines
+          const breakerWidth = 8;
+          const breakerHeight = breakerWidth;
+          const breakerFillColor =
+            topDecorators[index].topDecoData.breaker === "open"
+              ? "white"
+              : "black";
+          topDecoratorGroup
+            .append("rect")
+            .attr("class", "connectors")
+            .attr("id", `bus${nodeGroup.id}topDeco${index}Breaker`)
+            .attr(
+              "x",
+              () =>
+                Number($(`#bus${nodeGroup.id}topDeco${index}`).attr("x")) +
+                decoratorWidth / 2 -
+                breakerWidth / 2
+            )
+            .attr("y", y1 + breakerHeight * 0.8)
+            .attr("width", breakerWidth)
+            .attr("height", breakerHeight)
+            .attr("style", `fill:${breakerFillColor}`);
+        }
+
+        // Adding horizontal central connector for multiple top decorators.
         if (topDecoCount > 1) {
           topDecoratorGroup
             .append("line")
@@ -81,70 +131,22 @@ TopDecorators.prototype.decorate = function () {
             .attr(
               "x1",
               () =>
-                Number($(`#bus${nodeGroup.id}topDeco${index}`).attr("x")) +
+                Number($(`#bus${nodeGroup.id}topDeco0`).attr("x")) +
                 decoratorWidth / 2
             )
             .attr(
               "x2",
               () =>
-                Number($(`#bus${nodeGroup.id}topDeco${index}`).attr("x")) +
+                Number(
+                  $(`#bus${nodeGroup.id}topDeco${topDecoCount - 1}`).attr("x")
+                ) +
                 decoratorWidth / 2
             )
-            .attr("y1", y1)
-            .attr("y2", y2 + 0.5)
-            .attr(
-              "dx",
-              () => $(`#bus${nodeGroup.id}topDeco${index}`).attr("x") - 4
-            );
+            .attr("y1", -(R + LL))
+            .attr("y2", -(R + LL));
         }
-
-        // Adding breaker to vertical lines
-        const breakerWidth = 8;
-        const breakerHeight = breakerWidth;
-        const breakerFillColor =
-          topDecorators[index].topDecoData.breaker === "open"
-            ? "white"
-            : "black";
-        topDecoratorGroup
-          .append("rect")
-          .attr("class", "connectors")
-          .attr('id', `bus${nodeGroup.id}topDeco${index}Breaker`)
-          .attr(
-            "x",
-            () =>
-              Number($(`#bus${nodeGroup.id}topDeco${index}`).attr("x")) +
-              decoratorWidth / 2 -
-              breakerWidth / 2
-          )
-          .attr("y", y1 + breakerHeight * 0.8)
-          .attr("width", breakerWidth)
-          .attr("height", breakerHeight)
-          .attr("style", `fill:${breakerFillColor}`);
       }
-
-      // Adding horizontal central connector for multiple top decorators.
-      if (topDecoCount > 1) {
-        topDecoratorGroup
-          .append("line")
-          .attr("class", "connectors")
-          .attr(
-            "x1",
-            () =>
-              Number($(`#bus${nodeGroup.id}topDeco0`).attr("x")) +
-              decoratorWidth / 2
-          )
-          .attr(
-            "x2",
-            () =>
-              Number(
-                $(`#bus${nodeGroup.id}topDeco${topDecoCount - 1}`).attr("x")
-              ) +
-              decoratorWidth / 2
-          )
-          .attr("y1", -(R + LL))
-          .attr("y2", -(R + LL));
-      }
-    }
+    });
   });
 };
 
