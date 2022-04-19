6fe70c00920cf54ec0bf1f729c2ef2473671c360
Vicente Castro <vicente.castro@acelerex.com>
2022-04-19 15:33:11 -0400

    feat: toolbar tooltip

--- src/utils/Toolbar.js
+++ src/utils/Toolbar.js
@@ -9,15 +9,42 @@ const toolbarHtml = `
 </a>
 `;
 
-const addToolbar = () => {
+const tooltipText = {
+  "center-btn": "Reset view",
+};
+
+function showTooltip($event, btn) {
+  const tooltipHtml = `<div style="text-align: center; font-size: 12px">${tooltipText[btn]}</div>`;
+
+  d3.select("#tooltip")
+    .style("left", $event.pageX + "px")
+    .style("top", $event.pageY + "px")
+    .html(tooltipHtml)
+    .classed("hidden", false);
+}
+
+function hideTooltip() {
+  d3.select("#tooltip").classed("hidden", true);
+}
+
+function addToolbar() {
   // Add toolbar html
-  d3.select("#diagram-div").append("div").attr("id", "toolbar").attr("class", "toolbar-container");
+  d3.select("#diagram-div")
+    .append("div")
+    .attr("id", "toolbar")
+    .attr("class", "toolbar-container");
   d3.select("#toolbar").html(toolbarHtml);
+
+  d3.select("#center-btn")
+    .on("mouseover", ($event) => {
+      showTooltip($event, "center-btn");
+    })
+    .on("mouseout", () => hideTooltip());
 
   // Events handler
   d3.select("#center-btn").on("click", () => {
     drawGraph(inputEvent);
   });
-};
+}
 
 export default addToolbar;
