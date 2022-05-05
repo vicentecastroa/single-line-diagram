be9901786a5c9c905757f6b09309c961fa8b84eb
Vicente Castro <vicente.castro@acelerex.com>
2022-05-05 18:19:44 -0400

    feat: add decorators to branches

---
+++ src/Graph/Edges/Decorators/Decorators.js
@@ -0,0 +1,78 @@
+import * as d3 from "d3";
+import $ from "jquery";
+import SharedFunctionality from "../../../Views/baseView";
+
+// Icons
+import switchIcon from "../../../Icons/switch";
+import transformerIcon from "../../../Icons/transformer";
+
+const parser = new DOMParser();
+
+function Decorators(edges) {
+  this.edges = edges;
+
+  // Icons
+  this.icons = {
+    switch: switchIcon,
+    transformer: transformerIcon,
+  };
+}
+
+Decorators.prototype.decorate = function () {
+  this.decoratorWidth = 2.5 * SharedFunctionality.R;
+  this.edges._groups.forEach((g) => {
+    g.forEach((d) => {
+      const edge = d.__data__;
+      const decorators = edge.decorators;
+      const decoCount = decorators.length;
+
+      const decoratorGroup = d3
+        .select(d)
+        .append("g")
+        .attr("class", "decoratorGroup")
+        .attr("id", () => decorators.DOMID);
+
+      if (decoCount !== 0) {
+        // Only take first decorator
+        const decorator = decorators[0];
+        const icon = parser.parseFromString(
+          this.icons[decorator.resourceType],
+          "image/svg+xml"
+        );
+
+        const decoratorHTML = decoratorGroup
+          .node()
+          .appendChild(icon.documentElement);
+
+        d3.select(decoratorHTML)
+          .attr("width", this.decoratorWidth)
+          .attr("height", this.decoratorWidth)
+          .attr("id", () => `branch${edge.index}Deco${0}`)
+          .attr("y", 0)
+          .attr("x", 0);
+      }
+    });
+  });
+};
+
+Decorators.prototype.tick = function () {
+  d3.selectAll(".decoratorGroup")
+    .attr("transform", (d) => {
+      const x1 = Number($(`#${d.edgeData.DOMID}`).attr("x1"));
+      const x2 = Number($(`#${d.edgeData.DOMID}`).attr("x2"));
+      const y1 = Number($(`#${d.edgeData.DOMID}`).attr("y1"));
+      const y2 = Number($(`#${d.edgeData.DOMID}`).attr("y2"));
+
+      const rotationAngle = (Math.atan((y2 - y1) / (x2 - x1)) * 180) / Math.PI;
+
+      return `translate(${x1 + (x2 - x1) / 2 - this.decoratorWidth / 2},${
+        y1 + (y2 - y1) / 2 - this.decoratorWidth / 2
+      }) rotate(${rotationAngle}, ${this.decoratorWidth / 2}, ${
+        this.decoratorWidth / 2
+      })`;
+    })
+    .attr("zoomPointX", (d) => (d.source.x + d.target.x) / 2)
+    .attr("zoomPointY", (d) => (d.source.y + d.target.y) / 2);
+};
+
+export default Decorators;
---
+++ src/Icons/transformer.js
@@ -0,0 +1,14 @@
+const iconText = `
+<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 1 28 28">
+  <line x1="8" x2="22" y1="15" y2="15"  style="stroke:#fff;stroke-width:3;"/>
+  <g>
+    <path d="M 8 0 v 5 c 6 1 6 4 0 5 c 6 1 6 4 0 5 c 6 1 6 4 0 5 c 6 1 6 4 0 5 v 5" style="fill:none;stroke:#000;stroke-width:1" />
+    <line x1="0" x2="8" y1="15" y2="15"  stroke="black" />
+  </g>
+  <g transform="scale(-1,1) translate(-30,0)">
+    <path d="M 8 0 v 5 c 6 1 6 4 0 5 c 6 1 6 4 0 5 c 6 1 6 4 0 5 c 6 1 6 4 0 5 v 5" style="fill:none;stroke:#000;stroke-width:1" />
+    <line x1="0" x2="8" y1="15" y2="15"  stroke="#000" />
+  </g>
+</svg>`;
+
+export default iconText;
